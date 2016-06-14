41a93efb92c4a24bcbd63f01ad1ca6114a17daed
Syafrizal <syafrizal.frozen@yahoo.com>
2016-06-14 15:07:10 +0700

    <Add folder src>

---
+++ src/BasicTemplates.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MakeNew\SimpleMaker;
+
+
+class BasicTemplates
+{
+
+    public $tokens;
+
+    public function __construct(array $tokens)
+    {
+
+        $this->tokens = new FormatsTokens($tokens);
+
+    }
+
+
+    public function basicIndexTemplate()
+    {
+
+        $content = <<<EOD
+@extends('layouts.:::masterPage:::')
+
+@section('title')
+
+    <title>:::upperCaseModelName:::</title>
+
+@endsection
+
+@section('content')
+
+        <ol class='breadcrumb'>
+        <li><a href='/'>Home</a></li>
+        <li><a href=':::modelRoute:::'>:::upperCaseModelName:::</a></li>
+        </ol>
+
+<h1>This is your :::upperCaseModelName::: Index Page</h1>
+
+@endsection
+EOD;
+
+        return $this->tokens->formatTokens($content);
+
+    }
+
+}
---
+++ src/MakeSimpleView.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace MakeNew\SimpleMaker;
+
+use Illuminate\Console\Command;
+
+class MakeSimpleView extends Command
+{
+    use FormatsInput, BuildsTemplates, WritesViewFiles, HasParentAndChildAndSlug;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:views
+                           {ModelName}
+                           {MasterPage}
+                           {TemplateType}
+                           {Slug=false}
+                           {IndexOnly=false}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'create views';
+
+
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+
+        $this->setConfigFromInputs();
+
+
+
+       if ( $this->makeViewDirectory()->makeViewFiles($this->templateType) ) {
+
+            $this->sendSuccessMessage();
+
+           return;
+
+       }
+
+        $this->error('Oops, something went wrong!');
+
+
+    }
+
+    private function sendSuccessMessage()
+    {
+
+        $this->info('Views successfully created');
+
+    }
+
+
+}
